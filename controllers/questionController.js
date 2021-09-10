12d6c52cf9648cca7cd7fcdeb48ea3ce3f901b18
nonuabi <nonuabi@gmail.com>
2021-09-11 01:50:46 +0530

    add option controller

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -11,3 +11,40 @@ module.exports.create_ques = async function (req, res) {
       return res.status(400).send(err);
     });
 };
+
+//creating Option ROUTE
+module.exports.createOpt = function (req, res) {
+  var quest = Question.findOne({ _id: req.params.id });
+
+  if (quest) {
+    const Question = quest._id;
+    const Text = req.body.Text;
+
+    var option = Option.create({
+      Question,
+      Text,
+    });
+    var link_Vote =
+      req.protocol +
+      "://" +
+      req.headers.host +
+      "/options/" +
+      option._id +
+      "/add_vote";
+    option.link_Vote = link_Vote;
+    option.save();
+
+    //push the id
+    quest.options.push(option._id);
+    quest.save();
+
+    //succesfull create
+    console.log("Option Has Been Successfully  Added!");
+    res.data = option;
+    return;
+  } else {
+    //error
+    console.log("Question not Found!");
+    return res.redirect("back");
+  }
+};
