9d32fa277fe0885d91017cf0b166c08af7f374bb
nonuabi <nonuabi@gmail.com>
2021-09-11 18:22:51 +0530

    update create question controller

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,26 +1,30 @@
 const Question = require("../model/questionSchema");
 const Option = require("../model/optionSchema");
-//Create question route
+
+//CREATE QUESTION CONTROLLER
 module.exports.create_ques = async function (req, res) {
-  console.log("test create");
   const { question } = req.body;
-  Question.create({ Question: question })
-    .then((response) => {
-      return res.status(200).send(response);
-    })
-    .catch((err) => {
-      return res.status(400).send(err);
-    });
+  // CREATE QUESTION
+  const response = await Question.create({ Question: question });
+  if (response) {
+    return res.status(200).send(response);
+  } else {
+    return res.status(400).send(err);
+  }
 };
 
-//creating Option ROUTE
+
+
+
+// CREATE OPTION CONTROLER
 module.exports.createOpt = async function (req, res) {
+  // FINDING QUESTION BY ID
   const question_response = await Question.findOne({ _id: req.params.id });
-  console.log(question_response);
   if (question_response) {
     let question_id = question_response._id;
     let text = req.body.Text;
     if (text) {
+      // CREATE OPTION
       const option_response = await Option.create({
         Question: question_id,
         Text: text,
@@ -33,10 +37,12 @@ module.exports.createOpt = async function (req, res) {
           "/options/" +
           option_response._id +
           "/add_vote";
+        // UPDATING OPTION LINK VOTE
         option_response.link_Vote = link_Vote;
-        option_response.save();
+        await option_response.save();
+        // PUSHING OPTIONS FROM QUESTION SCHEMA
         question_response.Options.push(option_response._id);
-        question_response.save();
+        await question_response.save();
         return res.status(200).send(option_response);
       } else {
         return res.status(500).send("Option is not created");
@@ -47,61 +53,4 @@ module.exports.createOpt = async function (req, res) {
   } else {
     return res.status(404).send("Question Not Found");
   }
-  // .then((response) => {
-  //   const option_response = Option.create({
-  //     Question: question_id,
-  //     Text: text,
-  //   });
-  //   let link_Vote =
-  //     req.protocol +
-  //     "://" +
-  //     req.headers.host +
-  //     "/options/" +
-  //     option_response._id;
-  //   option_response.link_Vote = link_Vote;
-  //   await option_response.save();
-  //   console.log("check");
-  //   Option.findOne({ _id: option_response._id })
-  //     .then((response) => {
-  //       return res.status(200).send(response);
-  //     })
-  //     .catch((error) => {
-  //       return res.status(404).send(error);
-  //     });
-  // })
-  // .catch((error) => {
-  //   return res.status(500).send(error);
-  // });
-
-  // if (quest) {
-  //   const Question = quest._id;
-  //   const Text = req.body.Text;
-
-  //   var option = Option.create({
-  //     Question,
-  //     Text,
-  //   });
-  //   var link_Vote =
-  //     req.protocol +
-  //     "://" +
-  //     req.headers.host +
-  //     "/options/" +
-  //     option._id +
-  //     "/add_vote";
-  //   option.link_Vote = link_Vote;
-  //   option.save();
-
-  //   //push the id
-  //   quest.options.push(option._id);
-  //   quest.save();
-
-  //   //succesfull create
-  //   console.log("Option Has Been Successfully  Added!");
-  //   res.data = option;
-  //   return;
-  // } else {
-  //   //error
-  //   console.log("Question not Found!");
-  //   return res.redirect("back");
-  // }
 };
